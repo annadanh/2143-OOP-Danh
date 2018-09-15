6905bbd6b9acc556e8bd8d94630e895575da6df6
Anna <35544921+annadanh@users.noreply.github.com>
2018-09-14 21:53:58 -0500

    Update main.cpp

--- assignments/P01/main.cpp
+++ assignments/P01/main.cpp
@@ -1,10 +1,10 @@
 /**
+* Author: Anna Danh
 * Course: 2143-OOP
 * Assignment: P01
 * Purpose: The program uses a class that implements the functionality of a C++ vector using 
 * an array as the storage container. It grows and shrinks as items are added and removed.
 *
-* Author: Anna Danh
 * Date: 9/13/18
 */
 
@@ -20,10 +20,13 @@ int main() {
 
 	V.print();
 
+	// prints current size of vector
 	cout << "There are " << V.size() << " items in the vector.\n";
 
+	// attempt to remove from empty vector
 	V.pop_back();
 
+	// push values into vector
 	V.push_back(13);
 	V.push_back(31);
 	V.push_back(73);
@@ -31,19 +34,23 @@ int main() {
 	V.print();
 	cout << "\n";
 
-	
+	// push 10,000 random values into the vector
 	for(int i = 0; i < 10000; i++) {
 		V.push_back(rand() % 100);
 	}
 
+	// print current size of vector
 	cout << "There are " << V.size() << " items in the vector.\n";
 
+	// pop 9,998 items off the vector
 	for (int i = 0; i < 9998; i++) {
 		V.pop_back();
 	}
 
+	// print current size of vector
 	cout << "There are " << V.size() << " items in the vector.\n";
 
+	// print items in vector
 	V.print();
 	
 	system("pause");
